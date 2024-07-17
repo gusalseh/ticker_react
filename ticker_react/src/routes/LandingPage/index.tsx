5318ed9e6533a518c6a72b261759ef999681f01f
unknown <maroonever96@gmail.com>
2024-07-17 11:17:35 +0900

    add tailwindcss environment

--- ticker_react/src/routes/LandingPage/index.tsx
+++ ticker_react/src/routes/LandingPage/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Logo from "../../images/oglogo.png";
+import Slogan from "../../images/slogan.png";
 
 export default function LandingPage() {
-  return <div>랜딩페이지</div>;
+  return (
+    <div>
+      <img src={Logo}></img>
+      <img src={Slogan}></img>
+      <p className="text-blue-700">랜딩페이지</p>
+    </div>
+  );
 }
--- ticker_react/src/routes/Layout/index.tsx
+++ ticker_react/src/routes/Layout/index.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 
 export default function Layout() {
-  return (
-    <div>
-      {/* <div style={{ backgroundImage: `url("../images/logo.png")` }}></div> */}
-      <p>레이아웃</p>
-    </div>
-  );
+  return <div>레이아웃</div>;
 }
